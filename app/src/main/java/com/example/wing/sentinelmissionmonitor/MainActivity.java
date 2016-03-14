f245eb05db1477ccd3e38636210e41b8c3575fd9
Wingyuen Poon <wing@Wingyuens-MacBook-Pro.local>
2016-03-13 22:22:42 -0700

    Added Immersive Mode. Added Tablet Layouts

--- app/src/main/java/com/example/wing/sentinelmissionmonitor/MainActivity.java
+++ app/src/main/java/com/example/wing/sentinelmissionmonitor/MainActivity.java
@@ -28,6 +28,9 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         setContentView(R.layout.activity_main);
 
         findViews();
